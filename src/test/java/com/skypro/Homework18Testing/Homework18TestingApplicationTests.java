f0d755ff450bda96bd056cd44f45ebd70df53ba6
Denis Siliukov <abhem21rus@mail.ru>
2024-02-20 11:00:32 +0300

    Project initialization

---
+++ src/test/java/com/skypro/Homework18Testing/Homework18TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.Homework18Testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework18TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
