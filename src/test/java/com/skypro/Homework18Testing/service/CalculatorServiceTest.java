968b87f0a0983e7162bd269feaad14e0155fccd8
Denis Siliukov <abhem21rus@mail.ru>
2024-02-23 05:34:40 +0300

    Task completed

--- src/test/java/com/skypro/Homework18Testing/service/CalculatorServiceTest.java
+++ src/test/java/com/skypro/Homework18Testing/service/CalculatorServiceTest.java
@@ -1,7 +1,10 @@
 package com.skypro.Homework18Testing.service;
 
+import com.skypro.Homework18Testing.exception.DivisionByZeroException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,5 +25,20 @@ class CalculatorServiceTest {
         Assertions.assertEquals(8.0, subtractResult);
         Assertions.assertEquals(20.0, multiplyResult);
         Assertions.assertEquals(5.0, divideResult);
+    }
+    @Test
+    void calculateThrowsExceptionForDivisionByZero() {
+        Assertions.assertThrows(DivisionByZeroException.class, () ->
+                calculatorService.calculate(num1, 0, "divide"));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"10, 2, add, 12.0",
+            "10, 2, subtract, 8.0",
+            "10, 2, multiply, 20.0",
+            "10, 2, divide, 5.0"})
+    void calculateParameterizedTest(int num1, int num2, String operation, double expectedResult) {
+        double actualResult = calculatorService.calculate(num1, num2, operation);
+        assertEquals(expectedResult, actualResult);
     }
 }
